34649faaaf0aabbfc52d20a385bbc1be9bf974f5
Kunal Jadhav <94116481+Kunal-Jadhav-1@users.noreply.github.com>
2024-08-06 01:09:13 +0530

    Fixed Connect Button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
         <img src={logo} alt='logo' className='mx-2 w-10' />
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <a href="#contact" className='ml-4 px-1 py-1 bg-neutral-300 text-neutral-800 rounded-lg hover:bg-neutral-800 hover:text-neutral-300 text-sm'>Get in Touch</a>
+        <a href="#contact" className='ml-3 px-1 py-1 bg-neutral-300 text-neutral-800 rounded-lg hover:bg-neutral-800 hover:text-neutral-300 text-sm'>Connect</a>
         <a href={LINKEDIN} target='_blank' rel='noopener noreferrer'><FaLinkedin/></a>
         <a href={GITHUB} target='_blank' rel='noopener noreferrer'><FaGithub/></a>
         <a href={INSTA} target='_blank' rel='noopener noreferrer'><FaInstagram/></a>
